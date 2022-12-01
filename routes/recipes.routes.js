f7e01cbfaabee364312472ef08c66cd61f6a1070
stroligo <gabrielstroligo@gmail.com>
2022-12-01 11:48:45 +0000

    update

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -12,4 +12,72 @@ recipesRoute.get("/", async (req, res) => {
   }
 });
 
+// CREATE USER - MONGODB
+recipesRoute.post("/create-recipe", async (req, res) => {
+  try {
+    const form = req.body;
+
+    //quer criar um documento dentro da sua collection -> .create()
+    const newRecipe = await recipeModel.create(form);
+
+    return res.status(201).json(newRecipe);
+  } catch (error) {
+    console.log(error.errors);
+    return res.status(500).json(error.errors);
+  }
+});
+
+//R - READ - MONGODB
+recipesRoute.get("/all-recipes", async (req, res) => {
+  try {
+    const recipes = await recipeModel
+      .find({}, { __v: 0, updatedAt: 0 })
+      .sort({
+        title: 1,
+      })
+      .limit(100);
+
+    return res.status(200).json(recipes);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
+//D - Delete - MONGODB
+recipesRoute.delete("/del-recipe/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedRecipe = await recipeModel.findByIdAndDelete(id);
+
+    if (!deletedRecipe) {
+      return res.status(400).json({ msg: "Usuário não encontrado" });
+    }
+
+    return res.status(200).json(deletedRecipe);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
+//U - UPDATE - PUT - MONGODB
+recipesRoute.put("/edit-recipe/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const selectedRecipe = await recipeModel.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(selectedRecipe);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
 export default recipesRoute;
